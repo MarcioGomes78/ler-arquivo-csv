f4767670a36caeaee5003edb818661774dc6cf2b
MarcioGomes78 <mjgomes1978@gmail.com>
2022-07-21 20:04:51 -0300

    Implementação do File, Scanner e Exceptions

--- src/Program.java
+++ src/Program.java
@@ -10,18 +10,33 @@ public class Program {
 		// Só que o Scanner ele também funciona para ler arquivos, fazendo uma injeção de dependência
 		// utilizando o polimorfismo
 		// System.in representa a entrada padrão do java
-		// Só que podemos instanciar o Scanner baseado em uma stream de um arquivo ("file")
-		File file = new File("c:\\temp\\in.txt");
+		// Só que podemos instanciar o Scanner baseado em uma stream de um arquivo ("file ou o caminho")
+		File file = new File("c:\\temp\\in.txt"); 
 		Scanner sc = null;
+		
+		// Se der certo
 		try {
-			sc = new Scanner(file);
+			sc = new Scanner(file); // se der uma excessão de o Scanner não existe, vai para o catch
 			//A função hasNextLine retorna veerdadeiro se vc ainda não chegou no fim do arquivo
 			while (sc.hasNextLine()) {
-				
+				String line = sc.nextLine();
+				System.out.println(line);
 			}
 		}
+		
+		// se der errado
 		catch (IOException e) {
 			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+		// Finally serve para colocar fechamneto de recursos
+		//dando certo ou errado ele finaliza
+		finally {
+			//Se o Scannernão estiver aberto dará uma excessão de ponteiro nulo(NullPointerException)
+			// Para evitar o NPE colaca-se um if
+			if (sc != null) {
+				sc.close();
+			}
 			
 		}
 	}
